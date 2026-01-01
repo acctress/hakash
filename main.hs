34e13786b305a834db24a9edd7396f2133497f3e
blinx <111649405+acctress@users.noreply.github.com>
2026-01-01 12:56:45 +0000

    Update main.hs

--- main.hs
+++ main.hs
@@ -1,2 +1,15 @@
--- a simple function which checks if a char is an alphabetical character
-isAlpha c = c `elem` ['a'..'z']
+-- A simple function to check if a character is an alphabetical character
+isAlpha :: Char -> Bool
+isAlpha c = c `elem` ['a' .. 'z']
+
+-- For every number which is less than 10, replace with BOOM!, else BANG! - if it isn't a number get rid of it.
+boomBangs :: (Integral a) => [a] -> [String]
+boomBangs xs = [if x < 10 then "BOOM!" else "BANG!" | x <- xs, odd x]
+
+-- A custom function to retrieve the length of a list
+length' :: (Num a) => [t] -> a
+length' xs = sum [1 | _ <- xs]
+
+-- Returns a list of tuples which represent triangles
+triangles :: [(Integer, Integer, Integer)]
+triangles = [(a, b, c) | c <- [1 .. 10], b <- [1 .. 10], a <- [1 .. 10]]
